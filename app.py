9b7c322bf1447aa493fbbcb514ea6b0d0823e7f0
nithin7234 <benadictfranis@karunya.edu.in>
2025-11-13 23:43:10 +0530

    Added simple app.py script

---
+++ app.py
@@ -0,0 +1,9 @@
+# hello.py
+# Simple Python script for Git push demo
+
+def greet_user(name):
+    print(f"Hello, {name}! Welcome to Git Demo.")
+
+if __name__ == "__main__":
+    user_name = input("Enter your name: ")
+    greet_user(user_name)
